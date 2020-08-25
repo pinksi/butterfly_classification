41e71374674c6a1fd84d80b9af206da3e3360e41
pinkyy <pinky.sitikhu@docsumo.com>
2020-08-25 22:55:44 +0545

    adds simple template to upload image from frontend

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, request, jsonify
+from flask import render_template
 from loguru import logger
 
 from services.image_prediction import predict_image
@@ -12,7 +13,7 @@ def hello_world():
     return "Hello, World!"
 
 
-@app.route("/api/v1/predict/butterfly/", methods=["POST"])
+@app.route("/predict/butterfly/", methods=["POST"])
 def image_prediction():
     if request.headers["Content-Type"].startswith("multipart/form-data"):
         image = request.files.get("image", "")
@@ -27,7 +28,13 @@ def image_prediction():
         "predicted_class": data[0],
         "predicted_probability": data[1],
     }
-    return jsonify(res), 200
+    # return jsonify(res), 200
+    return render_template("index.html", title="Welcome", data=[res])
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Welcome")
 
 
 if __name__ == "__main__":
